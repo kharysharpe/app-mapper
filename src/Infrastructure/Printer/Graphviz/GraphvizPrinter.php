4f1b8a30b5b8bc1addd5ab607e96bc7926d51109
Herberto Graca <herberto.graca@gmail.com>
2018-11-19 11:18:31 +0100

    Add events to the graphviz printer

--- src/Infrastructure/Printer/Graphviz/GraphvizPrinter.php
+++ src/Infrastructure/Printer/Graphviz/GraphvizPrinter.php
@@ -21,11 +21,15 @@ use Fhaculty\Graph\Graph;
 use Graphp\GraphViz\GraphViz;
 use Hgraca\ContextMapper\Core\Component\Main\Domain\Component;
 use Hgraca\ContextMapper\Core\Component\Main\Domain\ContextMap;
+use Hgraca\ContextMapper\Core\Component\Main\Domain\DomainNodeInterface;
 use Hgraca\ContextMapper\Core\Port\Printer\PrinterInterface;
+use Hgraca\PhpExtension\String\StringService;
 
 final class GraphvizPrinter implements PrinterInterface
 {
     private const FORMAT = 'svg';
+
+    private $tmp = [];
 
     public function printToImage(ContextMap $contextMap): string
     {
@@ -47,11 +51,16 @@ final class GraphvizPrinter implements PrinterInterface
     private function printContextMap(ContextMap $contextMap): Graph
     {
         $graph = new Graph();
+        $graph->setAttribute('graphviz.graph.layout', 'circo');
+        $graph->setAttribute('graphviz.graph.rankdir', 'LR');
         $graph->setAttribute('graphviz.graph.labelloc', 't');
         $graph->setAttribute('graphviz.graph.label', $contextMap->getName());
         $graph->setAttribute('graphviz.graph.fontname', 'arial');
         $graph->setAttribute('graphviz.graph.fontsize', '30');
-        $graph->setAttribute('graphviz.graph.nodesep', '5');
+//        $graph->setAttribute('graphviz.graph.ranksep', '1.5');
+        $graph->setAttribute('graphviz.graph.nodesep', '2');
+        $graph->setAttribute('graphviz.graph.size', '60,60');
+        $graph->setAttribute('graphviz.graph.ratio', 'fill');
 
         $graph->setAttribute('graphviz.node.fontname', 'arial');
 
@@ -66,6 +75,22 @@ final class GraphvizPrinter implements PrinterInterface
             );
         }
 
+        // Only after adding all components, can we start adding the edges (links)
+        foreach ($contextMap->getComponentList() as $component) {
+            foreach ($component->getEventDispatcherList() as $eventDispatcher) {
+                $originComponentVertex = $graph->getVertex($component->getName());
+                foreach ($contextMap->getListenersOf($eventDispatcher) as $listener) {
+                    $destinationComponentVertex = $graph->getVertex($listener->getComponent()->getName());
+                    $eventEdge = $originComponentVertex->createEdgeTo($destinationComponentVertex);
+                    $eventEdge->setAttribute('graphviz.tailport', $this->createPortId($eventDispatcher));
+                    $eventEdge->setAttribute('graphviz.headport', $this->createPortId($listener));
+                    $eventEdge->setAttribute('graphviz.style', 'dashed');
+                    $eventEdge->setAttribute('graphviz.xlabel', $eventDispatcher->getEventCanonicalName());
+                    $eventEdge->setAttribute('graphviz.fontname', 'arial');
+                }
+            }
+        }
+
         return $graph;
     }
 
@@ -75,25 +100,48 @@ final class GraphvizPrinter implements PrinterInterface
             . '<tr><td BGCOLOR="Lavender"><b>' . $component->getName() . '</b></td></tr>';
 
         foreach ($component->getUseCaseList() as $useCase) {
-            $componentStr .= '<tr><td BGCOLOR="Lightsalmon" PORT="' . $useCase->getFullyQualifiedClassName() . '">'
-                . $useCase->getCanonicalClassName()
+            $componentStr .= '<tr><td BGCOLOR="Lightsalmon" PORT="' . $this->createPortId($useCase) . '">'
+                . $useCase->getCanonicalName()
                 . '</td></tr>';
+            $this->tmp[] = $useCase->getFullyQualifiedName();
         }
 
         foreach ($component->getListenerList() as $listener) {
-            $componentStr .= '<tr><td BGCOLOR="Honeydew" PORT="' . $listener->getFullyQualifiedName() . '">'
+            $componentStr .= '<tr><td BGCOLOR="Honeydew" PORT="' . $this->createPortId($listener) . '">'
                 . $listener->getCanonicalName()
                 . '</td></tr>';
+            $this->tmp[] = $listener->getFullyQualifiedName();
         }
 
         foreach ($component->getSubscriberList() as $subscriber) {
-            $componentStr .= '<tr><td BGCOLOR="Lightcyan" PORT="' . $subscriber->getFullyQualifiedName() . '">'
+            $componentStr .= '<tr><td BGCOLOR="Lightcyan" PORT="' . $this->createPortId($subscriber) . '">'
                 . $subscriber->getCanonicalName()
                 . '</td></tr>';
+            $this->tmp[] = $subscriber->getFullyQualifiedName();
         }
+
+        sort($this->tmp);
 
         $componentStr .= '</table>';
 
         return $componentStr;
+    }
+
+    private function createPortId(DomainNodeInterface $node): string
+    {
+        $id = StringService::replace('::', '.', $node->getFullyQualifiedName());
+        $id = StringService::replace('\\', '.', $id);
+        $id = ltrim($id, '.');
+        $useCaseTermination = 'Handler.';
+        if (StringService::contains($useCaseTermination, $id)) {
+            $id = mb_substr($id, 0, mb_strrpos($id, $useCaseTermination) + mb_strlen($useCaseTermination) - 1);
+        }
+        $listenerTermination = 'Listener.';
+        if (StringService::contains($listenerTermination, $id)) {
+            $id = mb_substr($id, 0, mb_strrpos($id, $listenerTermination) + mb_strlen($listenerTermination) - 1);
+        }
+        $id = StringService::replaceFromEnd('Handler', 'Command', $id);
+
+        return $id;
     }
 }
