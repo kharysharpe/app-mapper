713aecda16bca27a1ac4f1798fcc1d94ad347777
Herberto Graca <herberto.graca@gmail.com>
2018-12-22 10:25:33 +0000

    Share the host composer cache with the container

--- bin/docker-run.sh
+++ bin/docker-run.sh
@@ -4,6 +4,7 @@ docker run --rm \
   -e PHP_IDE_CONFIG="serverName=$(basename $(pwd))" \
   -v $(pwd)/build/php.ini:/usr/local/etc/php/php.ini \
   -v $(pwd)/build/xdebug.ini:/usr/local/etc/php/conf.d/xdebug.ini \
+  -v ~/.cache/composer:/.composer/cache/ \
   -v $(pwd):/$(basename $(pwd)) \
   -w /$(basename $(pwd)) \
   $@
