94da3b6486dfd7be9088fb9750875f834c9783f1
Herberto Graca <herberto.graca@gmail.com>
2018-12-23 10:16:16 +0000

    Refactor AssignmentFromConditionalTypeInjectorVisitor
    so it's more segregated and easier to maintain

--- src/Infrastructure/Parser/NikicPhpParser/Visitor/AssignmentFromConditionalTypeInjectorVisitor.php
+++ src/Infrastructure/Parser/NikicPhpParser/Visitor/AssignmentFromConditionalTypeInjectorVisitor.php
@@ -38,57 +38,107 @@ final class AssignmentFromConditionalTypeInjectorVisitor extends AbstractTypeInj
     public function leaveNode(Node $node): void
     {
         switch (true) {
-            case $node instanceof ConstFetch:
-                $name = (string) $node->name;
-                switch (true) {
-                    case $name === 'true' || $name === 'false':
-                        $this->addTypeToNode($node, new Type('bool'));
-                        break;
-                    case $name === 'null':
-                        $this->addTypeToNode($node, new Type('null'));
-                        break;
-                }
-                break;
-            case $node instanceof FuncCall:
-                if ($this->isNative($node)) {
-                    $this->addTypeToNode($node, new Type($this->getReturnType($node)));
-                }
-                break;
-            case $node instanceof Coalesce:
-                if (!self::hasTypeCollection($node->left) || !self::hasTypeCollection($node->right)) {
-                    // TODO stop ignoring unresolved and resolve all detected
-                    return;
-                }
-                $this->addTypeCollectionToNode($node, self::getTypeCollectionFromNode($node->left));
-                $this->addTypeCollectionToNode($node, self::getTypeCollectionFromNode($node->right));
-                break;
-            case $node instanceof Ternary:
-                if (!self::hasTypeCollection($node->if) || !self::hasTypeCollection($node->else)) {
-                    // TODO stop ignoring unresolved and resolve all detected
-                    return;
-                }
-                $this->addTypeCollectionToNode($node, self::getTypeCollectionFromNode($node->if));
-                $this->addTypeCollectionToNode($node, self::getTypeCollectionFromNode($node->else));
-                break;
-            case $node instanceof Variable:
-                $this->addCollectedVariableTypes($node);
-                break;
-            case $node instanceof Assign:
-                if (!self::hasTypeCollection($node->expr)) {
-                    // TODO stop ignoring unresolved and resolve all detected
-                    return;
-                }
-                $this->addTypeCollectionToNode($node->var, self::getTypeCollectionFromNode($node->expr));
-                $this->collectVariableTypes($node->var);
+            case $node instanceof Expr:
+                $this->leaveExprNode($node);
                 break;
             case $node instanceof ClassMethod:
-                $this->resetVariableTypeBuffer();
+                $this->leaveClassMethodNode();
                 break;
             case $node instanceof Class_:
-                $this->addPropertiesTypeToTheirDeclaration($node);
-                $this->resetPropertyTypeBuffer();
+                $this->leaveClassNode($node);
                 break;
         }
+    }
+
+    private function leaveExprNode(Expr $exprNode): void
+    {
+        switch (true) {
+            case $exprNode instanceof ConstFetch:
+                $this->leaveConstFetchNode($exprNode);
+                break;
+            case $exprNode instanceof FuncCall:
+                $this->leaveFuncCallNode($exprNode);
+                break;
+            case $exprNode instanceof Coalesce:
+                $this->leaveCoalesceNode($exprNode);
+                break;
+            case $exprNode instanceof Ternary:
+                $this->leaveTernaryNode($exprNode);
+                break;
+            case $exprNode instanceof Variable:
+                $this->leaveVariableNode($exprNode);
+                break;
+            case $exprNode instanceof Assign:
+                $this->leaveAssignNode($exprNode);
+                break;
+        }
+    }
+
+    private function leaveConstFetchNode(ConstFetch $constFetchNode): void
+    {
+        $name = (string) $constFetchNode->name;
+        switch (true) {
+            case $name === 'true' || $name === 'false':
+                $this->addTypeToNode($constFetchNode, new Type('bool'));
+                break;
+            case $name === 'null':
+                $this->addTypeToNode($constFetchNode, new Type('null'));
+                break;
+        }
+    }
+
+    private function leaveFuncCallNode(FuncCall $funcCallNode): void
+    {
+        if ($this->isNative($funcCallNode)) {
+            $this->addTypeToNode($funcCallNode, new Type($this->getReturnType($funcCallNode)));
+        }
+        // TODO if it's not native, try to find it in the AstMap
+    }
+
+    private function leaveCoalesceNode(Coalesce $coalesceNode): void
+    {
+        if (!self::hasTypeCollection($coalesceNode->left) || !self::hasTypeCollection($coalesceNode->right)) {
+            // TODO stop ignoring unresolved and resolve all detected
+            return;
+        }
+        $this->addTypeCollectionToNode($coalesceNode, self::getTypeCollectionFromNode($coalesceNode->left));
+        $this->addTypeCollectionToNode($coalesceNode, self::getTypeCollectionFromNode($coalesceNode->right));
+    }
+
+    private function leaveTernaryNode(Ternary $ternaryNode): void
+    {
+        if (!self::hasTypeCollection($ternaryNode->if) || !self::hasTypeCollection($ternaryNode->else)) {
+            // TODO stop ignoring unresolved and resolve all detected
+            return;
+        }
+        $this->addTypeCollectionToNode($ternaryNode, self::getTypeCollectionFromNode($ternaryNode->if));
+        $this->addTypeCollectionToNode($ternaryNode, self::getTypeCollectionFromNode($ternaryNode->else));
+    }
+
+    private function leaveVariableNode(Variable $variableNode): void
+    {
+        $this->addCollectedVariableTypes($variableNode);
+    }
+
+    private function leaveAssignNode(Assign $assignNode): void
+    {
+        if (!self::hasTypeCollection($assignNode->expr)) {
+            // TODO stop ignoring unresolved and resolve all detected
+            return;
+        }
+        $this->addTypeCollectionToNode($assignNode->var, self::getTypeCollectionFromNode($assignNode->expr));
+        $this->collectVariableTypes($assignNode->var);
+    }
+
+    private function leaveClassMethodNode(): void
+    {
+        $this->resetVariableTypeBuffer();
+    }
+
+    private function leaveClassNode(Class_ $classNode): void
+    {
+        $this->addPropertiesTypeToTheirDeclaration($classNode);
+        $this->resetPropertyTypeBuffer();
     }
 
     private function collectVariableTypes(Expr $var): void
