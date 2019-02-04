0b845eab5d0f62dceeb56b1cbde5c65eae75f2ae
Herberto Graca <herberto.graca@gmail.com>
2019-02-04 23:50:35 +0100

    Allow FuncCallNodeDecorator functions to return more than one type
    and add more native functions

--- src/Infrastructure/Parser/NikicPhpParser/Visitor/NodeDecorator/FuncCallNodeDecorator.php
+++ src/Infrastructure/Parser/NikicPhpParser/Visitor/NodeDecorator/FuncCallNodeDecorator.php
@@ -31,19 +31,65 @@ final class FuncCallNodeDecorator extends AbstractNodeDecorator implements Named
 {
     // FIXME should be possible to add to this in the config file
     private const FUNCTION_LIST = [
-        'array_unique' => ['return' => 'array'],
-        'count' => ['return' => 'int'],
-        'ctype_digit' => ['return' => 'bool'],
-        'filter' => ['return' => 'array'],
-        'implode' => ['return' => 'string'],
-        'iterator_to_array' => ['return' => 'array'],
-        'preg_replace' => ['return' => 'string'],
-        'reset' => ['return' => 'mixed'],
-        'sprintf' => ['return' => 'string'],
-        'strpbrk' => ['return' => 'string'],
-        'strrpos' => ['return' => 'string'],
-        'substr' => ['return' => 'string'],
-        'trigger_error' => ['return' => 'bool'],
+        'array_column' => ['return' => ['array']],
+        'array_filter' => ['return' => ['array']],
+        'array_flip' => ['return' => ['array']],
+        'array_intersect' => ['return' => ['array']],
+        'array_key_exists' => ['return' => ['bool']],
+        'array_keys' => ['return' => ['array']],
+        'array_map' => ['return' => ['array']],
+        'array_merge' => ['return' => ['array']],
+        'array_reduce' => ['return' => ['array']],
+        'array_search' => ['return' => ['bool', 'int', 'string']],
+        'array_shift' => ['return' => ['array']],
+        'array_slice' => ['return' => ['array']],
+        'array_unique' => ['return' => ['array']],
+        'array_values' => ['return' => ['array']],
+        'ceil' => ['return' => ['float']],
+        'count' => ['return' => ['int']],
+        'ctype_digit' => ['return' => ['bool']],
+        'date' => ['return' => ['bool', 'string']],
+        'end' => ['return' => ['mixed']],
+        'explode' => ['return' => ['array']],
+        'file_put_contents' => ['return' => ['int', 'bool']],
+        'filter' => ['return' => ['array']],
+        'floor' => ['return' => ['float']],
+        'get_class' => ['return' => ['string']],
+        'gettype' => ['return' => ['string']],
+        'implode' => ['return' => ['string']],
+        'in_array' => ['return' => ['bool']],
+        'invoke' => ['return' => ['array']], // functional-php
+        'invoker' => ['return' => ['callable']], // functional-php
+        'is_array' => ['return' => ['bool']],
+        'is_string' => ['return' => ['bool']],
+        'iterator_to_array' => ['return' => ['array']],
+        'json_encode' => ['return' => ['string']],
+        'json_decode' => ['return' => ['object', 'array']],
+        'map' => ['return' => ['array']], // functional-php
+        'max' => ['return' => ['int', 'float']],
+        'mb_strimwidth' => ['return' => ['string']],
+        'mb_substr' => ['return' => ['string']],
+        'md5' => ['return' => ['string']],
+        'parse_url' => ['return' => ['array', 'bool']],
+        'pathinfo' => ['return' => ['array', 'string']],
+        'preg_match' => ['return' => ['bool', 'int']],
+        'preg_replace' => ['return' => ['string']],
+        'reset' => ['return' => ['mixed']],
+        'round' => ['return' => ['float']],
+        'sprintf' => ['return' => ['string']],
+        'strpbrk' => ['return' => ['string']],
+        'strlen' => ['return' => ['int']],
+        'strrpos' => ['return' => ['string']],
+        'strtolower' => ['return' => ['string']],
+        'strtotime' => ['return' => ['int', 'bool']],
+        'strtoupper' => ['return' => ['string']],
+        'substr' => ['return' => ['string']],
+        'sys_get_temp_dir' => ['return' => ['string']],
+        'trigger_error' => ['return' => ['bool']],
+        'trim' => ['return' => ['string']],
+        'uasort' => ['return' => ['bool']],
+        'usort' => ['return' => ['bool']],
+        'var_export' => ['return' => ['null', 'string']],
     ];
 
     public function __construct(FuncCall $node, AbstractNodeDecorator $parentNode)
@@ -59,9 +105,12 @@ final class FuncCallNodeDecorator extends AbstractNodeDecorator implements Named
     public function resolveTypeCollection(): TypeCollection
     {
         if ($this->isKnownNativeFunction($this->node)) {
-            return new TypeCollection(
-                new Type(self::FUNCTION_LIST[$this->getFunctionName($this->node)]['return'])
-            );
+            $typeList = [];
+            foreach (self::FUNCTION_LIST[$this->getFunctionName($this->node)]['return'] as $returnType) {
+                $typeList[] = new Type($returnType);
+            }
+
+            return new TypeCollection(...$typeList);
         }
 
         $typeFromReflection = (string) (new ReflectionFunction($this->getFunctionName($this->node)))->getReturnType();
